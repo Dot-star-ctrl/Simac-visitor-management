fb0d808d05839154fef69612687d29575322e5ec
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2020-09-23 17:43:58 +0200

    Post, put and delete Visitor

--- simac/app/Http/Controllers/VisitorController.php
+++ simac/app/Http/Controllers/VisitorController.php
@@ -18,4 +18,31 @@ class VisitorController extends Controller
     {
         return new VisitorResourceCollection(Visitor::paginate());
     }
+
+    public function store(Request $request)
+    {
+        $request -> validate([
+            'first_name' => 'required',
+            'last_name'  => 'required',
+            'email'      => 'required',
+        ]);
+
+        $visitor = Visitor::create($request ->all());
+
+        return new VisitorResource($visitor);
+    }
+
+    public function update(Visitor $visitor, Request $request) : VisitorResource
+    {
+        $visitor -> update($request -> all());
+
+        return new VisitorResource($visitor);
+    }
+
+    public function destroy(Visitor $visitor)
+    {
+        $visitor -> delete();
+
+        return response() -> json();
+    }
 }
--- simac/app/Visitor.php
+++ simac/app/Visitor.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visitor extends Model
 {
-    
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+    ];
 }
