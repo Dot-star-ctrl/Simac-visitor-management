3bad96766d5eeeb4bfeea2602efcaf289f460423
daniel <you@example.com>
2021-01-19 00:21:01 +0100

    Fixed host and schedule tests

--- simac/database/factories/EmployeeFactory.php
+++ simac/database/factories/EmployeeFactory.php
@@ -32,5 +32,3 @@ class EmployeeFactory extends Factory
         ];
     }
 }
-
-
---
+++ simac/tests/Feature/ScheduleApiTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ScheduleApiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    use RefreshDatabase;
+
+    public function test_can_create_schedule()
+    {
+        $formData = [
+            'dateTime' => date('2020-01-14 02:11:55'),
+            'host_message' => 'HAHAHAH',
+        ];
+
+        $this->post(route('schedules.store'), $formData)
+             ->assertStatus(302);
+    }
+
+    public function test_can_update_schedule()
+    {
+        $this->withoutExceptionHandling();
+
+        $schedule = \App\Models\Schedule::factory()->create();
+
+        $updatedData = [
+            'dateTime' => date('2020-01-14 02:11:55'),
+            'host_message' => 'HAHAHAH',
+        ];
+
+        $this->put(route('schedules.update', $schedule), $updatedData)
+             ->assertStatus(200);
+    }
+
+    public function test_can_show_schedule()
+    {
+        $schedule = \App\Models\Schedule::factory()->create();
+
+        $this->get(route('schedules.show', $schedule->id))->assertStatus(200);
+    }
+
+    public function test_can_delete_schedule()
+    {
+        $schedule = \App\Models\Schedule::factory()->create();
+
+        $this->delete(route('schedules.destroy', $schedule->id))->assertStatus(200);
+    }
+
+    public function test_can_list_schedules()
+    {
+        $schedule = \App\Models\Schedule::factory()->count(3)->create();
+
+        $this->get(route('schedules.index'))
+        ->assertStatus(200);
+    }
+}
